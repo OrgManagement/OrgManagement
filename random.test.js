4bfe9ee5f6404a0885931f1782d1c7dd4cb783bd
zlmcdaniel <zlmcdaniel@gmail.com>
2022-03-23 11:03:08 -0400

    removing files

--- random.test.js
+++
@@ -1,7 +0,0 @@
-const mathOperations = require('./random');
-
-describe("Calculator tests", () => {
-    test('adding 1 + 2 should return 3', () => {
-      expect(mathOperations.sum(1, 2)).toBe(3);
-    });
-   })
